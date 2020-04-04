39000a96eb870ded43bdec086dfe1c8e44f83c03
LindsayKB <burtonlindsay70@yahoo.com>
2020-04-03 19:58:00 -0700

    Update script.js
    Change JS to support a single page app

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-  function show(id) {
-    document.getElementById(id).classList.add("show-menu");
-  }
-  function hide(id) {
-    document.getElementById(id).classList.remove("show-menu");
-  }
+window.onload=function(){
+	var lis = document.getElementById("navbar").getElementsByTagName('a');
+
+    for (var i=0; i<lis.length; i++) {
+        lis[i].addEventListener('click', fadeOut, false);
+    }
+
+    function fadeOut() {
+		var pageTitle = document.getElementById("page-title");
+		pageTitle.classList.remove("fade-in");
+		pageTitle.classList.add("fade-out");
+		pageTitle.textContent = "";
+		pageTitle.textContent += this.innerText;
+		pageTitle.classList.remove("fade-out");
+		pageTitle.classList.add("fade-in");	
+    }
+	
+	
+}
